e05c514e0b28194c5da4a731b6197587d303d6e6
Thales <thalesquadros2@gmail.com>
2026-02-06 01:49:25 -0300

    feat: create type pizza with struct

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main () {
-	
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Pizza struct {
+	id    uuid.UUID
+	name  string
+	price float64
+}
+
+func main() {
+	var pizzas = []Pizza{
+		{id: uuid.New(), name: "toscana", price: 49.9},
+		{id: uuid.New(), name: "marguerita", price: 30},
+		{id: uuid.New(), name: "quatro queijos", price: 45},
+	}
+	for _, value := range pizzas {
+		fmt.Println("Pizza cujo nome é", value.name)
+	}
 }
